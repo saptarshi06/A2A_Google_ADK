f044156d10c69423198b23d6663745babe356d4d
saptarshi06 <saptarshichattaraj714@gmail.com>
2026-04-23 17:14:40 +0530

    Updated

--- routes/routes.py
+++ routes/routes.py
@@ -1,6 +1,7 @@
 import uuid
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, json
 from services.orchestrator import ADKOrchestrator
+import re
 
 chat_bp = Blueprint('chat_bp', __name__)
 orchestrator = ADKOrchestrator()
@@ -19,11 +20,22 @@ def chat():
             else:
                 agent_to_call = "extractor_agent"
             
-            # FIXED: Don't pass session_id - it's generated internally
             output = orchestrator.run(
                 agent_name=agent_to_call,
                 user_input=user_query,
                 user_id="web_user_1"
             )
-            
+
+    # clean = re.sub(r"```json|```", "", output).strip()
+    # data = json.loads(clean)
+    # summary = data["summary"]
+
+    # match = re.search(r"\{.*\}", output, re.DOTALL)
+    # if match:
+    #     clean = match.group(0)
+    #     data = json.loads(clean)
+    #     summary = data.get("summary", "")
+    # else:
+    #     summary = "No JSON found"
+
     return render_template('index.html', output=output)
